bd9ad9382620a5264542a0ddec376f6ad792f8bb
Azimi2kht <seyedhamidazimidokht@gmail.com>
2024-08-03 23:32:27 +0330

    Feat: make the model more complex

--- src/model/models.py
+++ src/model/models.py
@@ -6,30 +6,47 @@ from torch import nn
 class CamculatorModel(nn.Module):
     def __init__(self):
         super(CamculatorModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 18)
+        self.conv1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=32,
+            kernel_size=3,
+            padding=1,
+        )
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=3,
+            padding=1,
+        )
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(
+            in_channels=64,
+            out_channels=128,
+            kernel_size=3,
+            padding=1,
+        )
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.dropout = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(128 * 3 * 3, 256)
+        self.fc2 = nn.Linear(256, 18)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
+        # conv - batch - relu - pool
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
 
-        x = self.conv2(x)
-        x = F.relu(x)
-
-        x = F.max_pool2d(x, 2)
-
-        x = self.dropout1(x)
-
-        x = torch.flatten(x, 1)
-
-        x = self.fc1(x)
-        x = F.relu(x)
-
-        x = self.dropout2(x)
+        # fully conneted
+        x = x.view(-1, 128 * 3 * 3)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
 
         output = F.log_softmax(x, dim=1)
